e12faf7687dcd34a560158a281c2bb203b72f58c
Lee Ryan <2solimanleeryan@gmail.com>
2022-11-13 06:41:07 +0800

    Configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,27 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
+    mode: 'development',
     entry: './src/index.js',
+    devtool: 'inline-source-map',
+    watch: true,
+    module: {
+        rules: [
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        }
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'To Do List',
+        }),
+    ],
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
-    watch: true,
-    
 }
