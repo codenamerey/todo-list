5150a3d8d7c320df6c917449d922c7d5e2aa5c39
Lee Ryan <2solimanleeryan@gmail.com>
2022-11-13 19:45:15 +0800

    Add task getter functionality

--- src/project.js
+++ src/project.js
@@ -1,13 +1,21 @@
-import {format, formatDistanceToNow} from 'date-fns';
+import {format} from 'date-fns';
 
 export default function projectFactory(name, due) {
     let projectName = name;
-    let tasks = [];
+    let tasks = [{title: "Default Task", desc: "This is an example task", due:"Tomorrow", prio:0}];
     const creationTime = format(new Date(), 'hh:mm, MM-dd-yyyy');
     let dueDate = format(due, 'LLLL dd, yyyy');
     const getDueDate = function() {
         return dueDate;
     }
 
-    return {getDueDate};
+    const getProjectName = function() {
+        return projectName;
+    }
+
+    const getTasks = function() {
+        return tasks;
+    }
+
+    return {getDueDate, getProjectName, getTasks};
 }
