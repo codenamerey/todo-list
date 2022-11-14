fd803043bcae8a13d0bd78530fa14acb92a9d286
Lee Ryan <2solimanleeryan@gmail.com>
2022-11-14 23:22:04 +0800

    Add in other event listeners for project adder

--- src/eventListeners.js
+++ src/eventListeners.js
@@ -1,13 +1,33 @@
 import PubSub from "./PubSub";
 
 export default (function eventListeners() {
-    function addEventListeners() {
-        const prjBtn = document.querySelector('#add-project-button');
-        prjBtn.addEventListener('click', broadcastAddPrj);
+    function addEventListeners(button, object) {
+        if(button.id == 'add-project-button') {
+            button.addEventListener('click', broadcastAddPrj);
+            return;          
+        }
 
+        if((button.id).match(/project-/g)) {
+            button.addEventListener('click', broadcastPrjClick.bind(null, object));
+            return;
+        }
+        // switch(button.id) {
+        //     case 'add-project-button':
+        //         button.addEventListener('click', broadcastAddPrj);
+        //         break;
+        //     case (button.id).match(/project-/g):
+        //         button.addEventListener('click', broadcastPrjClick.bind(null, object));
+        //         break;
+        // }
     }
     const broadcastAddPrj = function() {
-        PubSub.publish("AddPrjClick");
+        let projectName = prompt('Project Name: ');
+        PubSub.publish("AddPrjClick", projectName);
     }
+
+    const broadcastPrjClick = function(project) {
+        PubSub.publish("prjClick", project);
+    }
+
     return {addEventListeners}
 })();
