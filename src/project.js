6bc7894bcd59ef3a199ffa9b3f3c8b3736829333
Lee Ryan <2solimanleeryan@gmail.com>
2022-11-15 00:02:03 +0800

    Listen task button click, broadcast task added

--- src/project.js
+++ src/project.js
@@ -1,8 +1,10 @@
 import {format} from 'date-fns';
+import PubSub from './PubSub';
+import taskFactory from './task';
 
 export default function projectFactory(name, due) {
     let projectName = name;
-    let tasks = [{title: "Default Task", desc: "This is an example task", due:"Tomorrow", prio:0}];
+    let tasks = [{title: "Default Task", desc: "This is an example task", due:"Tomorrow", prio:0, getName: () => {return this.title;}}];
     const creationTime = format(new Date(), 'hh:mm, MM-dd-yyyy');
     let dueDate = format(due, 'LLLL dd, yyyy');
     const getDueDate = function() {
@@ -16,6 +18,14 @@ export default function projectFactory(name, due) {
     const getTasks = function() {
         return tasks;
     }
+
+    const addTask = function(name) {
+        let task = taskFactory(name, 'I HAVE NO IDEA', '2022-12-25', 0);
+        tasks.push(task);
+        PubSub.publish('taskAdded', tasks);
+    }
+
+    PubSub.subscribe("AddTaskClick", addTask);
 
     return {getDueDate, getProjectName, getTasks};
 }
