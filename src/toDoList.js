77920167ea4780ef343cb86fe3574a23c26a2d65
Lee Ryan <2solimanleeryan@gmail.com>
2022-11-20 13:13:38 +0800

    Recall projects on app initialization

--- src/toDoList.js
+++ src/toDoList.js
@@ -2,13 +2,23 @@ import PubSub from "./PubSub";
 import project from "./project";
 const toDoList = (function() {
     let projects = [];
+    let savedProjects = JSON.parse(localStorage.getItem('projects'));
     let current_project;
+    const recallProjects = function() {
+        savedProjects.forEach((savedProject) => {
+            addInitialProjects(savedProject.projectName);
+        });
+    }
 
-    const addProject = function(name) {
-        projects = (localStorage.getItem('projects')) ? JSON.parse(localStorage.getItem('projects')) : projects; 
-        let project_temp = new project(name, new Date("2022-12-25"));
+    const addInitialProjects = function(name) {
+        let project_temp = project(name, new Date("2022-12-25"));
         projects.push(project_temp);
         console.log(projects);
+    }
+
+    const addProject = function(name) { 
+        let project_temp = project(name, new Date("2022-12-25"));
+        projects.push(project_temp);
         localStorage.setItem('projects', JSON.stringify(projects));
         PubSub.publish("newProject", projects);
     }
@@ -24,9 +34,19 @@ const toDoList = (function() {
     const getProjects = function() {
         return projects;
     }
+    //recall saved projects
+    // if(savedProjects) {
+    //     savedProjects.forEach((savedProject) => {
+    //         let project_temp = project(savedProject.projectName, new Date("2022-12-25"));
+    //         projects.push(project_temp);
+    //         PubSub.publish("newProject", projects);
+    //     });
+    // }
 
     PubSub.subscribe("AddPrjClick", addProject);
     PubSub.subscribe("prjClick", setCurrentProject);
+    console.log(projects);
+    if(savedProjects) recallProjects();
     return {getCurrentProject, getProjects}
 })();
 
